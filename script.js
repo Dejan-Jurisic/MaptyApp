af1c2fc400b81472412e8dc6bc69a1b072f52077
Dejan Jurisic <dejan0jurisic@gmail.com>
2021-11-21 22:10:02 +0100

    Implemented a feature that move map to the position of the workout that was clicked in the sidebar

--- script.js
+++ script.js
@@ -62,9 +62,11 @@ class App {
         this.map;
         this.mapEvent;
         this.workouts = [];
+        this.mapZoomLevel = 13;
         this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
+        containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
     }
 
     _getPosition() {
@@ -85,7 +87,7 @@ class App {
 
         const cords = [latitude, longitude];
 
-        this.map = L.map('map').setView([51.505, -0.09], 13);
+        this.map = L.map('map').setView([51.505, -0.09], this.mapZoomLevel);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -213,6 +215,21 @@ class App {
             `;
         }
         form.insertAdjacentHTML('afterend', html);
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout')
+
+        if (!workoutEl) return;
+
+        const workout = this.workouts.find(work => work.id === workoutEl.dataset.id);
+        
+        this.map.setView(workout.coords, this.mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1
+            }
+        });
     }
 };
 
